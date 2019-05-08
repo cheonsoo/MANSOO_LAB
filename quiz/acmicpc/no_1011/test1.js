d6bcab34da2d88821b97a4917c8837e3cbfa3264
cheonsoo <mansoo1500@gmail.com>
2019-05-09 02:05:45 +0900

    .

---
+++ quiz/acmicpc/no_1011/test1.js
@@ -0,0 +1,23 @@
+// 45 50
+// 46 ~ 49
+// 45 46 48 49 50
+
+
+
+var x = 45;
+var y  = 50;
+
+var distance = y - x;
+var distance_half = 0;
+if ( distance % 2 == 1 ) {
+
+}
+
+var cnt = 0;
+var distance_moved = 0;
+while( start == distance ) {
+
+    for () {
+
+    }
+}
