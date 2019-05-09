ba926c25847fad6e9a3956e15dadcab4a514abb6
CheonsooPark-B2LiNK <cheonsoo@b2link.co.kr>
2019-05-09 20:27:06 +0900

    .

--- test/test.js
+++ test/test.js
@@ -1,19 +1,2 @@
-function factorial( i ) {
-    if ( i === 0 ) {
-        return 1;
-    }
 
-    return i * factorial( i - 1);
-}
-
-let a = 4;
-let b = 7;
-
-let sum = 0;
-for ( let i=0; i<4; i++ ) {
-	for ( let k=4; k>0; k-- ) {
-		sum += factorial( k-i );
-	}
-}
-
-console.log( sum );
+console.log( Math.pow( Math.floor( 3.49 ), 2 ) );
