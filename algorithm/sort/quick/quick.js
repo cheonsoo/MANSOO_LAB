c9c878df2c47886a4ebb365f7b482f89e35aa4e4
CheonsooPark-B2LiNK <cheonsoo@b2link.co.kr>
2019-05-21 19:10:58 +0900

    .

--- algorithm/sort/quick/quick.js
+++ algorithm/sort/quick/quick.js
@@ -3,18 +3,24 @@ var start = new Date().getTime();
 /**
  * 시간복잡도 : O(NLogN), 최악의 경우 : O(N^2)
  */
+function quickSort( _arr ) {
+    let arr = _arr.slice();
+    let arr_sorted = quick( arr );
+    return arr_sorted;
+}
+
 function quick( arr ) {
     
-    if ( arr.length < 2 ) {
+    if ( arr.length <= 1 ) {
         return arr;
     }
 
-    var pivot = arr.shift(); // first element
+    let pivot = arr.shift(); // first element
 
-    var left = [];
-    var right = [];
-    var new_arr = [];
-    for ( var i=0; i<arr.length; i++ ) {
+    let left = [];
+    let right = [];
+    let new_arr = [];
+    for ( let i=0; i<arr.length; i++ ) {
         if ( arr[ i ] <= pivot ) {
             left.push( arr[ i ] );
         } else {
@@ -25,11 +31,10 @@ function quick( arr ) {
     return new_arr.concat( quick( left ), pivot, quick( right ) );
 }
 
-var not_sorted = [ 8,3,4,1,5,6,2,7 ];
+let not_sorted = [ 8,3,4,1,5,6,2,7 ];
+let sorted = quickSort( not_sorted );
+
 console.log( not_sorted.join( "," ) );
-var sorted = quick( not_sorted );
-
-
 console.log( sorted.join( "," ) );
 
 console.log( `exe time : ${new Date().getTime() - start} ms` );
