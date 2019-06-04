4179f0d0996c71f4d22b5da5162f9ec462046fdf
cheonsoo <mansoo1500@gmail.com>
2019-06-05 01:58:52 +0900

    .

--- test/test.js
+++ test/test.js
@@ -1,10 +1,31 @@
-var hero = {
-    name : "cpark",
-    getName : function() {
-        return this.name;
-    }
-};
+// var coord = [ [ 1, 4 ], [ 3, 4 ], [ 3, 10 ] ];
 
-var a = hero.getName;
-console.log( a() );
-console.log( hero.getName() );
+// console.log( 5 & 13 );
+// console.log( 5 & 13 & 3 );
+// console.log( 1 ^ 3 ^ 3 );
+// console.log( 4 ^ 4 ^ 10 );
+
+// Integer to Binary
+var int = 4;
+console.log( int.toString(2) );
+
+// Binary to Integer
+console.log( parseInt( 1010, 2 ) );
+
+// 101 ^ 1001 = 1100
+// console.log( 5 ^ 9 );
+// console.log( 101 ^ 1001 );
+
+console.log( 4^10^4 );
+
+
+/*
+0100
+0100
+-----
+0000
+1010
+-----
+1010
+1110
+*/
